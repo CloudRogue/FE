a8c3ce50c405b89fef610d5af8a32a6d56bb41c1
OlMinJe <Ol.MinJe@gmail.com>
2025-12-29 12:27:40 +0900

    ♻️ refactor: Button 컴포넌트 forwardRef 전달 레이어 구현 #2

--- src/shared/ui/button.tsx
+++ src/shared/ui/button.tsx
@@ -1,9 +1,13 @@
 // Server Component
 
 import ButtonBase, { ButtonProps } from "@/src/shared/ui/button.base";
+import { forwardRef } from "react";
 
 export type { ButtonProps };
 
-export default function Button(props: ButtonProps) {
-  return <ButtonBase {...props} />;
-}
+const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
+  return <ButtonBase ref={ref} {...props} />;
+});
+
+Button.displayName = "Button";
+export default Button;
