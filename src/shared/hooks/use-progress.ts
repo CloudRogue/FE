6a19114aa49b36abfed884943f67f6d869cc4612
OlMinJe <Ol.MinJe@gmail.com>
2025-12-31 14:02:18 +0900

    🐛 BUG: useProgress 훅 lint 의존성 오류 수정  #26

--- src/shared/hooks/use-progress.ts
+++ src/shared/hooks/use-progress.ts
@@ -14,12 +14,12 @@ export const useProgress = (
   const { interval = 200, step = 5 } = options;
   const [value, setValue] = useState(0);
 
+  const finish = !isLoading && value > 0;
+
   useEffect(() => {
     let timer: NodeJS.Timeout;
 
     if (isLoading) {
-      setValue(0);
-
       timer = setInterval(() => {
         setValue((prev) => {
           if (prev >= 90) return prev + 0.1; // 90%부터는 천천히 증가
@@ -27,16 +27,20 @@ export const useProgress = (
           return prev + step; // 설정한 step 만큼 증가
         });
       }, interval);
-    } else if (!isLoading && value > 0) {
-      setValue(100);
-      timer = setTimeout(() => setValue(0), 1000);
+    } else if (finish) {
+      timer = setTimeout(() => {
+        setValue((prev) => {
+          if (prev === 100) return 0;
+          return 100;
+        });
+      }, 0);
     }
 
     return () => {
       clearInterval(timer);
       clearTimeout(timer);
     };
-  }, [isLoading, interval, step]);
+  }, [isLoading, interval, step, finish]);
 
   return value;
 };
