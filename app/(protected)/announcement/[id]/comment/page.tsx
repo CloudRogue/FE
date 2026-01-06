34fc4f11ea58b3728c72a7b909495dd2bcc0bc31
OlMinJe <Ol.MinJe@gmail.com>
2026-01-06 17:43:35 +0900

    💄UI: 변경된 기능 명세서 토대로 공통 레이아웃 1차 구현 #50

---
+++ app/(protected)/announcement/[id]/comment/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <main>공고 상세 댓글 페이지 입니다.</main>;
+}
--- src/shared/layout/header.tsx
+++ src/shared/layout/header.tsx
@@ -1,7 +1,12 @@
-import Link from "next/link";
 import Button from "@/src/shared/ui/button";
+import { Bell, Heart } from "lucide-react";
+import Link from "next/link";
 
-type HeaderVariant = "unauthenticated" | "authenticated";
+type HeaderVariant =
+  | "unauthenticated"
+  | "authenticated"
+  | "announcement"
+  | "mypage";
 
 type HeaderProps = {
   variant: HeaderVariant;
@@ -19,15 +24,30 @@ export default function Header({ variant }: HeaderProps) {
           <span className="text-base">🏘️ 집착</span>
         </Link>
 
-        {variant === "unauthenticated" ? (
+        {variant === "unauthenticated" && (
           <Link href="/login" aria-label="로그인 페이지로 이동">
             <Button className="h-9 px-4 text-sm font-medium">로그인</Button>
           </Link>
-        ) : (
-          <Link href="/mypage" aria-label="마이페이지로 이동">
-            <Button className="h-9 px-4 text-sm font-medium">마이페이지</Button>
+        )}
+
+        {variant === "authenticated" && (
+          <div className="flex items-center">
+            <Bell size={24} />
+            <Link href="/mypage" aria-label="마이페이지로 이동">
+              <Button className="h-9 px-4 text-sm font-medium">
+                마이페이지
+              </Button>
+            </Link>
+          </div>
+        )}
+
+        {variant === "announcement" && (
+          <Link href="mypage">
+            <Heart size={24} />
           </Link>
         )}
+
+        {variant === "mypage" && <Link href="mypage">마이페이지 헤더</Link>}
       </div>
     </header>
   );
