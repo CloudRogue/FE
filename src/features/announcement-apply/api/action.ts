d5184546b6a971374fdd3dcd95a84f58d5664c31
OlMinJe <Ol.MinJe@gmail.com>
2026-01-07 14:07:24 +0900

    ♻️ REFACTOR: fetch 모듈로 변경 #42

--- src/features/announcement-apply/api/action.ts
+++ src/features/announcement-apply/api/action.ts
@@ -1,32 +1,12 @@
 "use server";
 
-import { ApiErrorSchema } from "@/src/entities/announcement-detail/model/announcement.types";
+import { Api } from "@/src/shared/api/api";
+import z from "zod";
 
 export async function postOutboundLog(announcementId: number) {
-  try {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_API_BASE_URL}/announcements/${announcementId}/outbound`,
-      {
-        method: "POST",
-      },
-    );
-
-    if (!res.ok) {
-      const errorData = await res.json();
-      const validatedError = ApiErrorSchema.safeParse(errorData);
-
-      if (validatedError.success) {
-        throw new Error(validatedError.data.message);
-      }
-      throw new Error("신청 기록 중 오류가 발생했습니다.");
-    }
-
-    return { success: true };
-  } catch (error) {
-    console.error("Outbound API Error:", error);
-    return {
-      success: false,
-      message: error instanceof Error ? error.message : "알 수 없는 에러",
-    };
-  }
+  return await Api.post(
+    `/announcements/${announcementId}/outbound`,
+    z.unknown(),
+    {},
+  );
 }
