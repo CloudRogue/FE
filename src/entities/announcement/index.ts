afd1e650bfd6564afd3107a9719bd1677293f257
Hyunji <hyunhyun0096@naver.com>
2026-01-08 01:06:19 +0900

    feat: index 설정

---
+++ src/entities/announcement/index.ts
@@ -0,0 +1,12 @@
+export { AnnouncementCard } from "./ui/announcement-card";
+export { announcementQueries } from "./api/queries";
+export type {
+  Announcement,
+  AnnouncementFilterParams,
+  AnnouncementListResponse,
+} from "./model/types";
+export {
+  AnnouncementSchema,
+  AnnouncementFilterParamsSchema,
+  AnnouncementListResponseSchema,
+} from "./model/types";
