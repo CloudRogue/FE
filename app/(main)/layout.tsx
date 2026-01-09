314f13ce58a817e9c020c293e4a5aed7fe42038b
OlMinJe <Ol.MinJe@gmail.com>
2026-01-09 11:44:20 +0900

    💡DOCS: 메인 레이아웃 헤더 pathname 토대로 분기처리 진행

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,6 +2,7 @@ import BottomNav from "@/src/shared/layout/bottom-nav";
 import Header from "@/src/shared/layout/header";
 import { ReactNode } from "react";
 
+// TODO: pathname 토대로 분기처리 필요
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="min-h-dvh flex flex-col">
