7a5631c9aec4410f3a1513643e6738bcafe91857
Hyunji <hyunhyun0096@naver.com>
2026-01-09 10:31:17 +0900

    feat(entities/user): 유저 스토어 및 프로필 조회 훅 구현

--- src/entities/user/index.ts
+++ src/entities/user/index.ts
@@ -1,3 +1,4 @@
 export { useUserStore } from "@/src/entities/user/model/user-store";
 export { getProfile } from "@/src/entities/user/api/get-profile";
 export type { User } from "@/src/entities/user/model/types";
+export { useUser } from "@/src/entities/user/lib/use-user";
