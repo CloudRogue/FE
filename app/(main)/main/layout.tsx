f3d9fc8c941972b953182f1c1589b6eaccb9cddf
OlMinJe <Ol.MinJe@gmail.com>
2026-01-09 11:37:16 +0900

    🎨 STYLE: 변경된 와이어프레임에 맞춰서 폴더 구조 변경

--- app/(main)/main/layout.tsx
+++ app/(main)/main/layout.tsx
@@ -5,8 +5,9 @@ import { ReactNode } from "react";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="min-h-dvh flex flex-col">
-      <Header variant="authenticated" />
-      <main className="container mx-auto px-4 py-8 flex-1 flex">
+      {/* TODO: 게스트 - unauthenticated, 회원 authenticated */}
+      <Header variant="unauthenticated" />
+      <main /*className="container mx-auto px-4 py-8 flex-1 flex"*/>
         {children}
       </main>
       <BottomNav />
