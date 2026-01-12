4bc8b7284aaea2865597feb88deef5efaf0ae5e5
Hyunji <hyunhyun0096@naver.com>
2026-01-12 16:27:44 +0900

    chore : index.ts 로 내보내기

--- src/features/filter-announcements/index.ts
+++ src/features/filter-announcements/index.ts
@@ -1,9 +1,10 @@
 export { RegionFilter } from "@/src/features/filter-announcements/ui/region-filter";
 export { PublisherFilter } from "@/src/features/filter-announcements/ui/publisher-filter";
 export { HousingTypeFilter } from "@/src/features/filter-announcements/ui/housing-filter";
-
+export { SortSelector } from "@/src/features/filter-announcements/ui/sort-selector";
 export { AnnouncementFilter } from "@/src/features/filter-announcements/ui/announce-filter";
 
+export { FilterTriggerBar } from "@/src/features/filter-announcements/ui/filter-trigger-bar";
 export { useFilterStore } from "@/src/features/filter-announcements/model/use-filter-store";
 
 export {
