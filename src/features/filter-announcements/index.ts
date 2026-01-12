aa2f501847c78dcb93ff8885d2761c5b0b77d9f9
Hyunji <hyunhyun0096@naver.com>
2026-01-12 13:23:47 +0900

    chore : index.ts 추가

---
+++ src/features/filter-announcements/index.ts
@@ -0,0 +1,13 @@
+export { RegionFilter } from "@/src/features/filter-announcements/ui/region-filter";
+export { PublisherFilter } from "@/src/features/filter-announcements/ui/publisher-filter";
+export { HousingTypeFilter } from "@/src/features/filter-announcements/ui/housing-filter";
+
+export { AnnouncementFilter } from "@/src/features/filter-announcements/ui/announce-filter";
+
+export { useFilterStore } from "@/src/features/filter-announcements/model/use-filter-store";
+
+export {
+  MOCK_REGIONS,
+  MOCK_PUBLISHERS,
+  MOCK_HOUSING_TYPES,
+} from "@/src/features/filter-announcements/model/constants";
