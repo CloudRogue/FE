d1f4cfadd707b51030c9b9454d8f78727a16e9c2
Hyunji <hyunhyun0096@naver.com>
2026-01-15 16:32:42 +0900

    :art: feat(UI): 반응형 적용

---
+++ src/shared/layout/responsive-layout.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from "react";
+
+interface ResponsiveLayoutProps {
+  children: ReactNode;
+}
+
+export function ResponsiveLayout({ children }: ResponsiveLayoutProps) {
+  return (
+    <div className="min-h-screen w-full bg-slate-50 flex justify-center items-center">
+      <div
+        className="
+        w-full max-w-[768px] min-h-screen bg-white shadow-2xl flex flex-col
+        
+        justify-end items-center
+        
+        md:justify-center md:min-h-[1024px]
+      "
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
