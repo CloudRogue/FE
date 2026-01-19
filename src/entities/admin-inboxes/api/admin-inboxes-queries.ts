4be20ecd5283c7a34bfe84e6fc0e6acfcea3b4f7
OlMinJe <Ol.MinJe@gmail.com>
2026-01-20 03:11:54 +0900

    🐛 FIX: 검토 전 공고 API 호출 주소 오류 수정 #95

--- src/entities/admin-inboxes/api/admin-inboxes-queries.ts
+++ src/entities/admin-inboxes/api/admin-inboxes-queries.ts
@@ -1,6 +1,6 @@
-import { Api } from "@/src/shared/api/api";
 import { AdminInboxesResponseSchema } from "@/src/entities/admin-inboxes/model/admin-inboxes-type";
+import { Api } from "@/src/shared/api/api";
 
 export function getAdminInboxes() {
-  return Api.get("/api/admin/inboxes", AdminInboxesResponseSchema);
+  return Api.get("/admin/inboxes", AdminInboxesResponseSchema);
 }
