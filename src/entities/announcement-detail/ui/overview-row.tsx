84143d74f99179f6c1d1dd92135891c919dcff30
OlMinJe <Ol.MinJe@gmail.com>
2026-01-19 14:35:52 +0900

    ♻️ REFACTOR: 공고 개요 지역 뱃지 수정 #115

--- src/entities/announcement-detail/ui/overview-row.tsx
+++ src/entities/announcement-detail/ui/overview-row.tsx
@@ -2,22 +2,33 @@ import Popover from "@/src/shared/ui/popover";
 
 interface SummaryRowProps {
   label: string;
-  value: string;
+  value: React.ReactNode;
   children?: React.ReactNode;
+  isOpen?: boolean;
+  onClose?: () => void;
 }
 
-export function OverviewRow({ label, value, children }: SummaryRowProps) {
+export function OverviewRow({
+  label,
+  value,
+  children,
+  isOpen,
+  onClose,
+}: SummaryRowProps) {
   return (
-    <div className="flex justify-between py-2 text-[15px] ">
-      <span className="text-gray-500">{label}</span>
-      <div>
-        <span className="text-black text-right">{value}</span>
+    <div className="flex justify-between items-center py-2 text-[15px]">
+      <span className="text-gray-400 font-medium">{label}</span>
+      <div className="flex items-center gap-2">
+        <span className="text-gray-900 font-semibold">{value}</span>
         {children && (
           <Popover
             align="right"
-            className="w-75 p-6 rounded-3xl border-none shadow-[0_8px_30px_rgb(0,0,0,0.12)]"
+            center={true}
+            isOpen={isOpen}
+            onClose={onClose}
+            className="w-[calc(100%-40px)] max-w-100 p-6 rounded-2xl border-none shadow-[0_10px_40px_rgba(0,0,0,0.15)]"
             trigger={
-              <span className="px-2 py-1 text-[11px] font-bold text-white bg-[#437CFF] rounded-sm cursor-pointer hover:bg-blue-600 transition-colors">
+              <span className="px-2 py-1 text-[11px] font-bold text-white bg-[#437CFF] rounded-md cursor-pointer">
                 전체보기
               </span>
             }
