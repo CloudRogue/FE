a97f27d72991db0ee38e769ce446a51c3c810deb
Hyunji <hyunhyun0096@naver.com>
2026-01-20 22:13:01 +0900

    :fire: refactor: api명세 바탕으로 user 업데이트

--- src/entities/user/model/use-user-store.ts
+++ src/entities/user/model/use-user-store.ts
@@ -1,13 +1,16 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
-import type { UserState, User } from "@/src/entities/user/model/user.types";
+import type {
+  UserState,
+  ProfileBasic,
+} from "@/src/entities/user/model/user.types";
 
 export const useUserStore = create<UserState>()(
   devtools(
     (set) => ({
       user: null,
       isLoggedIn: false,
-      setUserInfo: (user: User) =>
+      setUserInfo: (user: ProfileBasic) =>
         set({ user, isLoggedIn: true }, false, "user/setUserInfo"),
       clearUser: () =>
         set({ user: null, isLoggedIn: false }, false, "user/clearUser"),
