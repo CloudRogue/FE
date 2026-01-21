067b6467639416447d4865fca95d4b8e602482f6
Hyunji <hyunhyun0096@naver.com>
2026-01-21 17:17:16 +0900

    fix: 빌드오류 해결

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  turbopack: {},
   async rewrites() {
     return [
       {
@@ -9,12 +10,12 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"],
     });
-
     return config;
   },
 };
