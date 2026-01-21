ebd8f320eae0e170d33a35498cede50fa8c8b171
Hyunji <hyunhyun0096@naver.com>
2026-01-21 22:41:48 +0900

    :hot: fix:로그아웃 인증흐름개선

--- src/entities/user/api/use-user.queries.ts
+++ src/entities/user/api/use-user.queries.ts
@@ -3,6 +3,7 @@ import {
   getProfileBasic,
   getProfileDetail,
 } from "@/src/entities/user/api/user.action";
+import { useUserStore } from "@/src/entities/user";
 
 export const USER_QUERY_KEYS = {
   all: ["user"] as const,
@@ -11,17 +12,21 @@ export const USER_QUERY_KEYS = {
 };
 
 export const useGetProfileBasic = () => {
+  const isLoggedIn = useUserStore((s) => s.isLoggedIn);
   return useQuery({
     queryKey: USER_QUERY_KEYS.profile(),
     queryFn: getProfileBasic,
+    enabled: isLoggedIn,
     staleTime: 1000 * 60 * 5,
   });
 };
 
 export const useGetProfileDetail = () => {
+  const isLoggedIn = useUserStore((s) => s.isLoggedIn);
   return useQuery({
     queryKey: USER_QUERY_KEYS.detail(),
     queryFn: getProfileDetail,
+    enabled: isLoggedIn,
     staleTime: 1000 * 60 * 5,
   });
 };
