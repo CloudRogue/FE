8e29d4c34b4e914a800e74a4b0a434c81845b795
OlMinJe <Ol.MinJe@gmail.com>
2026-01-26 00:13:32 +0900

    💄 UI: 루트 레이아웃 main 요소에 Pb 추가

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-1 flex flex-col min-h-[calc(100dvh-9rem)] bg-gray-bg">
+      <main className="flex-1 flex flex-col min-h-[calc(100dvh-9rem)] bg-gray-bg pb-17">
         {children}
       </main>
       <BottomNav />
