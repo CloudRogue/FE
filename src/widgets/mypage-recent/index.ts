18b0a870d4d871ddc448a0d80448235f45b4940e
OlMinJe <Ol.MinJe@gmail.com>
2026-01-25 20:38:28 +0900

    💄 UI: 최근 열람 공고 스켈레톤 UI 적용 #255

---
+++ src/widgets/mypage-recent/index.ts
@@ -0,0 +1,2 @@
+export { RecentAnnouncementList } from "@/src/widgets/mypage-recent/ui/recent-announcement-list";
+export { RecentCardSkeleton } from "@/src/widgets/mypage-recent/ui/recent-card-skeleton";
